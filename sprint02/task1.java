5dfb1e177f20dd13456b21635339437d1061ef90
Vladislav Siverskiy <79045792+VladyslavSiverskiy@users.noreply.github.com>
2023-07-05 14:25:26 +0300

    Create task1.java

---
+++ sprint02/task1.java
@@ -0,0 +1,35 @@
+class Person{
+    int age;
+    String healthInfo;
+    String name;
+
+    public Person(int age, String healthInfo, String name) {
+        this.age = age;
+        this.healthInfo = healthInfo;
+        this.name = name;
+    }
+
+    public String getHealthStatus(){ return name + " " + healthInfo; }
+}
+
+
+class Child extends Person{
+
+    private String childIDNumber;
+
+    public Child(int age, String healthInfo, String name, String childIDNumber) {
+        super(age, healthInfo, name);
+        this.childIDNumber = childIDNumber;
+    }
+}
+
+
+class Adult extends Person{
+
+    private String passportNumber;
+
+    public Adult(int age, String healthInfo, String name, String passportNumber) {
+        super(age, healthInfo, name);
+        this.passportNumber = passportNumber;
+    }
+}
