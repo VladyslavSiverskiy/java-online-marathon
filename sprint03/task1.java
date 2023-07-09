eacb214128b5bed6bbf957d009e2557cdf97660b
Vladislav Siverskiy <79045792+VladyslavSiverskiy@users.noreply.github.com>
2023-07-09 10:55:32 +0300

    Create task1.java

---
+++ sprint03/task1.java
@@ -0,0 +1,79 @@
+class Pizza {
+    private String cheese;
+    private String meat;
+    private String seafood;
+    private String vegetable;
+    private String mushroom;
+
+    private Pizza() { }
+    
+    public String getCheese() {
+        return cheese;
+    }
+    public String getMeat() {
+        return meat;
+    }
+    public String getSeafood() {
+        return seafood;
+    }
+    public String getVegetable() {
+        return vegetable;
+    }
+    public String getMushroom() {
+        return mushroom;
+    }
+    
+    public static PizzaBuilder base() {
+        return new PizzaBuilder();
+    }
+    
+     public static class PizzaBuilder {
+        private Pizza pizza;
+
+        private PizzaBuilder() {
+
+        }
+
+        public PizzaBuilder addCheese(String cheese) {
+            pizza.cheese = cheese;
+            return this;
+        }
+
+        public PizzaBuilder addMeat(String meat) {
+            pizza.meat = meat;
+            return this;
+        }
+
+        public PizzaBuilder addSeafood(String seafood) {
+            pizza.seafood = seafood;
+            return this;
+        }
+
+        public PizzaBuilder addVegetable(String vegetable) {
+            pizza.vegetable = vegetable;
+            return this;
+        }
+
+        public PizzaBuilder addMushroom(String mushroom) {
+            pizza.mushroom = mushroom;
+            return this;
+        }
+
+		public Pizza build(){
+			pizza = new Pizza();
+			return pizza;
+		}
+    }
+    
+}
+
+class Oven {
+    public static Pizza cook() {
+        Pizza.PizzaBuilder builder = Pizza.base();
+		Pizza pizza = builder.build();
+		builder.addCheese("Some cheese");
+		builder.addMeat("Some meat");
+		builder.addVegetable("Tomatoes");
+        return pizza;    
+    }
+}
